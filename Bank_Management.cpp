8c4aaaed2854d3e19ac76e8734066715d43db387
Harsh324 <harshravi324@gmail.com>
2021-12-05 11:44:05 +0530

    method definitions added

--- Bank_Management.cpp
+++ Bank_Management.cpp
@@ -2,21 +2,22 @@
 
 class Account
 {
-    double Balance;
+    
     protected:
-        void Deposit()
+        double Balance;
+        void Deposit(double Amount)
+        {
+            Balance += Amount;
+        }
+
+        bool Withdraw(double Amount)
         {
             //
         }
 
-        void Withdraw()
+        double Check_balance()
         {
-            //
-        }
-
-        void Check_balance()
-        {
-            //
+            return Balance;
         }
 
 };
@@ -25,9 +26,23 @@ class Checking_Account : protected Account
 {
     double Credit_limit;
     protected:
-        void Over_draft()
+        bool Withdraw(double Amount)
         {
-            //
+            if((Balance - Amount) >= 0)
+            {
+                Balance = Balance - Amount;
+                return true;
+            }
+            else if((Balance - Amount) >= -1 * Credit_limit)
+            {
+                Balance = Balance - Amount;
+                return true;
+            }
+            else
+            {
+                printf("You have crossed you credit limit . you can not withdraw. credit limit is %lf , while blance is %lf",Credit_limit , Check_balance());
+                return false;
+            }
         }
 };
 
@@ -37,13 +52,38 @@ class Saving_account : protected Account
     protected:
         void Interest_Addition()
         {
-            //
+            double SI = ( Balance * Interest_rate ) / 1200;
+            Balance += SI;
         }
 };
 
 class Loan : protected Account
 {
     double Principle , Interest_Rate;
-    int Month;
+    int Loan_duration;
+
+    double Interest()
+    {
+        double SI = ( Principle * Interest_Rate ) / 1200;
+        return SI;
+    }
+
+    bool Loan_debit()
+    {
+        while(Loan_duration)
+        {
+            if(Withdraw(Interest()))
+            {
+                Principle = Principle - Interest();
+                return true;
+            }
+            else
+                return false;
+        }
+    } 
+};
+
+class Interface : protected Account
+{
     
 };
