5c068ddd04422bded977ccbcd513f5f34f33d787
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 <harshravi324@gmail.com>
2022-03-04 23:37:38 +0530

    loanAccount class Added

--- Java_projects/Account.java
+++ Java_projects/Account.java
@@ -1,5 +1,8 @@
 package Java_projects;
 
+/**
+ * Abstract class acting as base class
+ */
 abstract class Account 
 {
     private String Name;
@@ -23,9 +26,13 @@ abstract class Account
 
 }
 
+/**
+ * CheckingAccount having a credit limit
+ */
 class CheckingAccount extends Account
 {
     private double creditlimit;
+    private double Balance1;
 
     public CheckingAccount(String name, String Phone, String Email, String Address)
     {
@@ -35,6 +42,79 @@ class CheckingAccount extends Account
     @Override
     void deposit(double D) 
     {
+        Balance1++;
+    }
+
+    @Override
+    void withdraw(double W)
+    {
+        if(Balance1 - W >= creditlimit)
+        {
+            Balance1 -= W;
+        } 
+        else
+            System.out.println("Can't Initiate Withdrawl ! , Credit limit reached");
+    }
+
+    @Override
+    double balanceEnquiry()
+    {
+        return Balance1;
+    }
+}
+
+class SavingAccount extends Account
+{
+    private double saving_Balance;
+    private double Interest_Rate;
+    public SavingAccount(String name, String Phone, String Email, String Address)
+    {
+        super(name, Phone, Email, Address);
+    }
+
+    @Override
+    void deposit(double D) 
+    {
+        saving_Balance += D;
+        
+    }
+
+    @Override
+    void withdraw(double W)
+    {
+        if(saving_Balance - W >= 0)
+        {
+            saving_Balance -= W;
+        }
+        else
+            System.out.println("Can't Initiate Withdrawl ! , Cfredit limit reached");
+        
+    }
+
+    @Override
+    double balanceEnquiry()
+    {
+        return saving_Balance;
+    }
+
+    public void Add_Interest()
+    {
+        double Interest = (saving_Balance) * (Interest_Rate / 100);
+        saving_Balance += Interest;
+    }
+}
+
+class loanAccount extends Account
+{
+    
+    public loanAccount(String name, String Phone, String Email, String Address)
+    {
+        super(name, Phone, Email, Address);
+    }
+
+    @Override
+    void deposit(double D) {
+        // TODO Auto-generated method stub
         
     }
 
@@ -46,9 +126,8 @@ class CheckingAccount extends Account
 
     @Override
     double balanceEnquiry() {
-        return Balance;
+        // TODO Auto-generated method stub
+        return 0;
     }
-
-    
 
 }
