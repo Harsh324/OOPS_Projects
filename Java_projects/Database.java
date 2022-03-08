e94cfce4cafd3d2f893b82969dabbea3cffa0de8
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 <harshravi324@gmail.com>
2022-03-08 15:37:34 +0530

    Database file changed

--- Java_projects/Database.java
+++ Java_projects/Database.java
@@ -15,8 +15,186 @@ class Database
 
 class Admin_Database
 {
-    
+
+    public void Add_Details(String Detail) throws FileNotFoundException, IOException
+    {
+        String Path = "Java_projects/DataBase/Customers/Details/";
+
+
+        List<String> Lst = new ArrayList<>();
+        Lst = Get_metadata();
+        //Updating Customer ID
+        String Str = Lst.get(0);
+
+        String Path1 = "Java_projects/DataBase/Customers/" + "2022_CM_" + Str;
+        File folder = new File(Path1);
+        folder.mkdir();
+
+        String Path2 = Path1 + "/Details";
+        String Path3 = Path1 + "/Accounts";
+        String Path4 = Path1 + "/Transactions";
+
+        folder = new File(Path2);
+        folder.mkdir();
+
+        folder = new File(Path3);
+        folder.mkdir();
+
+        folder = new File(Path4);
+        folder.mkdir();
+        
+        
+        
+        Path = Path2 + "2022_CM_" + Str + ".csv";
+        File file = new File(Path);
+        file.createNewFile();
+
+        String Str1 = "Name;Phone;Email;Address";
+
+        BufferedWriter out = new BufferedWriter(new FileWriter(Path, true));
+        out.write(Str1);
+        out.write(Detail);
+        out.close();
+
+
+    }
+
+    public void Add_Account(String Acc_Details, String Customer_ID, String Account_Type) throws IOException
+    {
+        String Path = "Java_projects/DataBase/Customers/" + Customer_ID + "/Accounts/";
+        
+        List<String> Lst = new ArrayList<>();
+        Lst = Get_metadata();
+        //Updating Customer ID
+        String Str = Lst.get(1);
+        
+        Path = Path + "2022_AC_" + Str + ".csv";
+        File file = new File(Path);
+        file.createNewFile();
+
+        String Str1 = "AccountType;Balance;";
+
+        BufferedWriter out = new BufferedWriter(new FileWriter(Path, true));
+        out.write(Str1);
+        out.write(Acc_Details);
+        out.close();
+
+        if(Account_Type == "Saving")
+        {
+            String Path5 = "Java_projects/DataBase/Accounts/Saving_Accounts.csv";
+            BufferedWriter out1 = new BufferedWriter(new FileWriter(Path, true));
+            out1.write(Acc_Details);
+        }
+        else if(Account_Type == "Loan")
+        {
+            String Path5 = "Java_projects/DataBase/Accounts/Saving_Accounts.csv";
+            BufferedWriter out1 = new BufferedWriter(new FileWriter(Path, true));
+            out1.write(Acc_Details);
+        }
+        else if(Account_Type == "Checking")
+        {
+            String Path5 = "Java_projects/DataBase/Accounts/Saving_Accounts.csv";
+            BufferedWriter out1 = new BufferedWriter(new FileWriter(Path, true));
+            out1.write(Acc_Details);
+        }
+        
+    }
+
+
+    public void Add_Transaction(String Trans_Details, String Customer_ID) throws FileNotFoundException, IOException
+    {
+        String Path = "Java_projects/DataBase/Customers/" + Customer_ID + "/Transactions/";
+        
+        List<String> Lst = new ArrayList<>();
+        Lst = Get_metadata();
+        //Updating Customer ID
+        String Str = Lst.get(2);
+
+        Path = Path + "2022_TS_" + Str + ".csv";
+
+        File file = new File(Path);
+        file.createNewFile();
+
+        String Str1 = "";
+
+        BufferedWriter out = new BufferedWriter(new FileWriter(Path, true));
+        out.write(Str1);
+        out.write(Trans_Details);
+        out.close();
+
+    }
+
+    public List Get_metadata() throws FileNotFoundException, IOException
+    {
+        String Path = "Java_projects/DataBase/Metadata/metadata.csv";
+        try (BufferedReader br = new BufferedReader(new FileReader(Path))) {
+
+            List<List<String>> result = new ArrayList<>();
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(";");
+                result.add(Arrays.asList(values));
+            }
+            return result;
+            //System.out.println(result);
+        }
+    }
+
+    public List Get_Accounts() throws FileNotFoundException, IOException
+    {
+        List<Object> Result = new ArrayList<>();
+        String Path = "Java_projects/DataBase/Customers/Accounts/";
+        File folder = new File(Path);
+        File[] listOfFiles = folder.listFiles();
+        for(File file : listOfFiles)
+        {
+            try (BufferedReader br = new BufferedReader(new FileReader(file))) 
+            {
+
+                List<List<String>> result = new ArrayList<>();
+                String line;
+                while ((line = br.readLine()) != null) {
+                    String[] values = line.split(";");
+                    Result.add(Arrays.asList(values));
+                }
+                //Result.add(result);
+                //System.out.println(result);
+            }
+        }
+        
+        return Result;
+    }
+
+    public List Get_Transactions() throws FileNotFoundException, IOException
+    {
+        List<Object> Result = new ArrayList<>();
+        String Path = "Java_projects/DataBase/Customers/Accounts/";
+        File folder = new File(Path);
+        File[] listOfFiles = folder.listFiles();
+        for(File file : listOfFiles)
+        {
+            try (BufferedReader br = new BufferedReader(new FileReader(file))) 
+            {
+
+                List<List<String>> result = new ArrayList<>();
+                String line;
+                while ((line = br.readLine()) != null) {
+                    String[] values = line.split(";");
+                    Result.add(Arrays.asList(values));
+                }
+                //Result.add(result);
+                //System.out.println(result);
+            }
+        }
+        
+        return Result;
+    }
 }
+
+
+
+
+
 
 class Customer_Database
 {
@@ -63,9 +241,9 @@ class Customer_Database
                 String line;
                 while ((line = br.readLine()) != null) {
                     String[] values = line.split(";");
-                    result.add(Arrays.asList(values));
+                    Result.add(Arrays.asList(values));
                 }
-                Result.add(result);
+                //Result.add(result);
                 //System.out.println(result);
             }
         }
@@ -90,9 +268,9 @@ class Customer_Database
                 String line;
                 while ((line = br.readLine()) != null) {
                     String[] values = line.split(";");
-                    result.add(Arrays.asList(values));
+                    Result.add(Arrays.asList(values));
                 }
-                Result.add(result);
+                //Result.add(result);
                 //System.out.println(result);
             }
         }
