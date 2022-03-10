c4ed71025e0e76902f837e7aceeba2f86e1c5236
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 <harshravi324@gmail.com>
2022-03-10 19:54:25 +0530

    Interace work in progress

--- Java_projects/Interface.java
+++ Java_projects/Interface.java
@@ -1,11 +1,12 @@
 package Java_projects;
 
+import java.io.IOException;
 import java.util.*;
 
 
 class Interface
 {
-    public static void main(String[] args) 
+    public static void main(String[] args) throws IOException 
     {
 
         Scanner sc = new Scanner(System.in);
@@ -21,37 +22,53 @@ class Interface
                 Admin Adm = new Admin();
                 while(true)
                 {
-                    System.out.println("\nEnter 1 to open Account\nEnter 2 to get All Account Details : ");
+                    System.out.println(
+                        "\nEnter 1 to open Account\nEnter 2 to get All Account Details\nEnter 3 to Print Customer details\n"
+                        + "Enter 4 to print Account Details\nEnter 5 to print Transaction Details : "
+                        );
                     Num = sc.nextInt();
                     if(Num == 1)
                     {
                         // System.out.println("Enter the Customer details");
                         // Adm.Get_Details();
-                        int Val = 2;
-                        while(Val != 1)
+
+                        if(Adm.Validate())
                         {
-                            System.out.println("\nEnter the Customer details\n");
-                            Adm.Get_Details();
-                            System.out.println("\nReview the details once\n");
-                            Adm.Review_Details();
-                            System.out.println("\nEnter 1 to confirm\nEnter 2 to Enter again");
-                            Val = sc.nextInt();
+                            Adm.Take_Account_Type();
+                        }
+
+                        else
+                        {
+                            int Val = 2;
+                            while(Val != 1)
+                            {
+                                System.out.println("\nEnter the Customer details\n");
+                                Adm.Take_Details();
+                                System.out.println("\nReview the details once\n");
+                                Adm.Review_Details();
+                                System.out.println("\nEnter 1 to confirm\nEnter 2 to Enter again");
+                                Val = sc.nextInt();
+                            }
                         }
                         Adm.Open_Account();
+
                     }
+
+                    
                     else if(Num == 2)
                     {
                         System.out.println("\nPrinting all the details\n");
-                        Adm.Print_All_details();
+                        Adm.Print_Account_Details();
                     }
                 }
                 
             }
-            else if(Num == 2)System.out.println("New Account is opened");
+            else if(Num == 2)
             {
                 Customer Cstm = new Customer();
                 Cstm.Login();
                 Account Ac;
+                
             }
         }
 
